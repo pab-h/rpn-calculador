4d09cb708e7208193aceacd5c3c910cd8d4a7f4f
pab-h <dev.pab.2020@gmail.com>
2023-08-14 09:21:42 -0300

    cli

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#include "headers/rpn.h"
+
+int main(int argc, char* args[]) {
+
+    if (argc != 2) {
+        printf("You only need parser the expression in Reverse Polish Notation!\n");
+        printf("About Reverse Polish Notation: https://en.wikipedia.org/wiki/Reverse_Polish_notation\n");
+
+        return 1;
+    }
+
+    char* expression = args[1];
+    float result = solve_expression(expression);
+
+    printf("Your expression: %s\n", expression);
+    printf("Result: %f\n", result);
 
     return 0; 
 }
